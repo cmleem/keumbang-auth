081588fcda58e1220d33a9f298ba9612aacef15f
Chae Min Lim <coals722@naver.com>
2024-09-09 17:39:55 +0900

    setting: initial

---
+++ src/main/java/backend/keumbangauth/KeumbangAuthApplication.java
@@ -0,0 +1,13 @@
+package backend.keumbangauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeumbangAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeumbangAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/keumbangauth/KeumbangAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.keumbangauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeumbangAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
